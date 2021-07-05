6fcbf85535f535b75c3e5091aa7db71fc786d4d9
LAPTOP-M723371O\9708j <betty970823@naver.com>
2021-07-05 09:16:04 +0900

    21.07.05 JE

--- routes/admin.js
+++ routes/admin.js
@@ -7,12 +7,18 @@ dotenv.config();
 
 const USER = process.env.RPC_USER;
 const PASS = process.env.RPC_PASSWORD;
-const PORT = process.env.RPC_PORT;
-const ACCOUNT = process.env.ACCOUNT;
-const ID_STRING = process.env.ID_STRING;
+const PORT = 9612;
+const ACCOUNT = "jeje";
+const ID_STRING = "jemerald";
 const headers = {
-  "content-type" : "text/plain;"
+  "content-type": "text/plain;"
 };
+// const PORT = process.env.RPC_PORT;
+// const ACCOUNT = process.env.ACCOUNT;
+// const ID_STRING = process.env.ID_STRING;
+// const headers = {
+//   "content-type" : "text/plain;"
+// };
 
 router.get('/', async function (req, res) {
     client.query('SELECT * FROM stellar.Users WHERE seed >= 5000', (err, data)=>{
