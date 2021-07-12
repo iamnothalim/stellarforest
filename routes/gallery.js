d932d3ea27ef4a44502d4a1c7208cf9ab33d5aa9
DESKTOP-HK4VH6P\dla67 <byeonggoon>
2021-07-12 21:38:21 +0900

    7월 12일 저녁.

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,15 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+const session = require('../lib/session');
 const pool = require('../lib/mariadb');
-
+router.use(session);
 /* GET home page. */
 router.get('/', async function (req, res) {
     try {
+        const user =req.session.userid
         const conn = await pool.getConnection();
         const topTenUserIds = await conn.query('select userid, tree from Users order by tree desc limit 10');
         conn.release();
-        res.render('gallery',{topTenUserIds});
+        res.render('gallery',{topTenUserIds, user});
     } catch (e) {
         console.log(e.message);
     }
